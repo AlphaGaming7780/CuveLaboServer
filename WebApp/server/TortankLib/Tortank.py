705eb2c31ed7a185d975661af0e2d375b9b6d32e
AlphaGaming7780 <69048433+AlphaGaming7780@users.noreply.github.com>
2025-03-06 00:08:07 +0100

    Fixe for mobile and Better tortank lib

---
+++ WebApp/server/TortankLib/Tortank.py
@@ -0,0 +1,67 @@
+from gpiozero import Motor
+
+class Tortank(object):
+    
+    TORTANK_WATER_LEVEL_MAX = 0.95
+
+    _motor1 : Motor
+    _motor2 : Motor
+
+    _motor1Speed : int
+    _motor2Speed : int
+
+    def __init__(self):
+        self._motor1 = Motor(16, 17)
+        self._motor2 = Motor(23, 24)
+        pass
+    
+    def SetMotor1Speed(self, speed : int):
+        """
+        Set the speed of the motor 1.\n
+        `speed` : int, take a number between 0 and 1. 0 stop, 1 full speed.
+        """
+        self._motor1Speed = speed
+        self._motor1.forward(speed)
+        pass
+
+    def GetMotor1Speed(self) -> int:
+        """
+        Return the speed of the motor 1.\n
+        `return` : int, a number between 0 and 1. 0 stop, 1 full speed.
+        """
+        return self._motor1Speed
+
+    def SetMotor2Speed(self, speed : int):
+        """
+        Set the speed of the motor 2.\n
+        `speed` : int, take a number between 0 and 1. 0 stop, 1 full speed.
+        """
+        self._motor2Speed = speed
+        self._motor2.forward(speed)
+        pass
+    
+    def GetMotor2Speed(self) -> int:
+        """
+        Return the speed of the motor 2.\n
+        `return` : int, a number between 0 and 1. 0 stop, 1 full speed.
+        """
+        return self._motor2Speed
+
+    def GetWaterLevelCuve1(self) -> int:
+        return 0.5
+    
+    def GetWaterLevelCuve2(self) -> int:
+        return 0.025
+    
+    def GetWaterLevelCuve3(self) -> int:
+        return 0.975
+    
+    def GetHeigestWaterLevel(self) -> int:
+
+        v1 = self.GetWaterLevelCuve1()
+        v2 = self.GetWaterLevelCuve2()
+        v3 = self.GetWaterLevelCuve3()
+        
+        return max(v1, v2, v3)
+
+    pass
--- WebApp/server/main.py
+++ WebApp/server/main.py
@@ -1,8 +1,11 @@
 # Tuto : Flask with React
 # https://nitratine.net/blog/post/how-to-serve-a-react-app-from-a-python-server/ 
 
+import threading
 from flask import Flask, jsonify, render_template, request
+from TortankLib.Tortank import Tortank
 
+tortank : Tortank = Tortank()
 app = Flask(__name__, static_url_path='') ## Changer le __name__ en un vrais nom ?
 
 @app.route('/')
@@ -15,6 +18,10 @@ def SendWaterLevel():
     waterLevel = []
 
     # Envoyer les données de Tortank
+    # waterLevel.append(tortank.GetWaterLevelCuve1())
+    # waterLevel.append(tortank.GetWaterLevelCuve2())
+    # waterLevel.append(tortank.GetWaterLevelCuve3())
+
     waterLevel.append(0.5)
     waterLevel.append(0.05)
     waterLevel.append(0.975)
@@ -26,7 +33,16 @@ def SendWaterLevel():
 if __name__ == '__main__':
 
 
-    # webServerThread = threading.Thread(target=lambda: app.run(host='0.0.0.0', debug=True, use_reloader=False))
-    # webServerThread.start()
+    webServerThread = threading.Thread(target=lambda: app.run(host='0.0.0.0', debug=True, use_reloader=False))
+    # webServerThread = threading.Thread(target=lambda: app.run(debug=True, use_reloader=False))
+    webServerThread.start()
+    # app.run(use_reloader=True)
 
-    app.run(use_reloader=True)
+    # while(True):
+
+    #     waterLevel = tortank.GetHeigestWaterLevel()
+
+    #     if(waterLevel >= tortank.TORTANK_WATER_LEVEL_MAX) :
+    #         tortank.SetMotor1Speed(0)
+    #         tortank.SetMotor2Speed(0)
+    #     pass
