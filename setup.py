6af9a42cb872776794efc37d02d5be3a9fd8eed2
AlphaGaming7780 <69048433+AlphaGaming7780@users.noreply.github.com>
2025-03-06 12:06:31 +0100

    S

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(name="Tortank WebServer",
         'smbus2',
         'gpiozero'
         # Add other dependencies here
-    ]
-    # packages=find_packages()
+    ],
+    packages=['TortankWebServer']
 )
