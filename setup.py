f07cbadb3d25169e2b476e3144853a89b71f92a6
AlphaGaming7780 <69048433+AlphaGaming7780@users.noreply.github.com>
2025-03-06 14:36:37 +0100

    d8

--- setup.py
+++ setup.py
@@ -1,23 +1,31 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
+NAME = 'TortankWebServer'
+VERSION = '0.0.1' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
 setup(
-    name="TortankWebServer",
-    version="0.1.0",
-    description="An example package for showing how to pip install from a git repo",
-    license='MIT',
+    # the name must match the folder name 'verysimplemodule'
+    name=NAME, 
+    version=VERSION,
     author="Triton Supreme",
-    authoer_email="matias.codesal@gmail.com",
+    author_email="<youremail@email.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
     url='https://github.com/AlphaGaming7780/TortankWebServer',
     install_requires=[
         'Flask',
         'smbus2',
         'gpiozero'
     ],
-    packages=['TortankWebServer'],
     entry_points={
         'console_scripts': 
         [
             'TortankWebServer=TortankWebServer.__main__:main' 
         ] 
-    }
+    },
+    include_dirs=["static"],
 )
