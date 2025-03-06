1f29e933f4d27e114f3a00bd65f3663bb1509415
AlphaGaming7780 <69048433+AlphaGaming7780@users.noreply.github.com>
2025-03-06 12:00:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from distutils.core import setup, find_packages
 
 setup(name="Tortank WebServer",
-      version="0.1.0",
-      description="An example package for showing how to pip install from a git repo",
-      license='MIT',
-      author="Matias Codesal",
-      authoer_email="matias.codesal@gmail.com",
-      url='https://github.com/matiascodesal/git-for-pip-example',
-      install_requires=[
+    version="0.1.0",
+    description="An example package for showing how to pip install from a git repo",
+    license='MIT',
+    author="Matias Codesal",
+    authoer_email="matias.codesal@gmail.com",
+    url='https://github.com/matiascodesal/git-for-pip-example',
+    install_requires=[
         'Flask',
         'threading',
         'Enum',
         'smbus2',
         'gpiozero'
         # Add other dependencies here
-    ],
-      packages=find_packages()
+    ]
+    # packages=find_packages()
 )
