635371997bb60b4efa6ab43730a4bf0a5b668a16
AlphaGaming7780 <69048433+AlphaGaming7780@users.noreply.github.com>
2025-03-06 14:14:10 +0100

    Change folder struct

--- WebPage/postbuild.js
+++ WebPage/postbuild.js
@@ -2,7 +2,7 @@ var path = require('path');
 const fs = require("fs");
 
 const targetSource = './build'; // Relative path to copy files from
-const targetDestination = '../server/static'; // Relative path to copy files to
+const targetDestination = '../TortankWebServer/static'; // Relative path to copy files to
 
 /**
  * Remove directory recursively
