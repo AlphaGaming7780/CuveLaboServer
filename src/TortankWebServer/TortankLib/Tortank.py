20ca5b2f8d3079ef45b3e43ec135e623d61163fc
Math0XK <145477623+Math0XK@users.noreply.github.com>
2025-03-09 03:24:19 +0100

    Jeff Bezos

--- src/TortankWebServer/TortankLib/Tortank.py
+++ src/TortankWebServer/TortankLib/Tortank.py
@@ -45,8 +45,12 @@ class Tortank(object):
 		# 4 = +-1.024v
 		# 8 = +-0.512v
 		# 16 = +-0.256v
-		self.ads.gain = 1
+		
+        # On peut pas utiliser le gain de 4.069v car la valeur max des capteurs dépasse 4.069v
+		# ---------->Verifier si les calculs de maximums sont toujours correctes<-------------
+		self.ads.gain = 2/3
 
+        # DEPRECATED
 		# self.ads = ADS1115()s
 		# self.ads.setMode(ADS1115_MODE.SINGLESHOT)
 		# self.ads.setGain(ADS1115_PGA.ADS1115_PGA_4P096)
@@ -87,24 +91,52 @@ class Tortank(object):
 	def ConvertRawWaterValue(self, rawADC : int) : 
 
 		# Si la uint16 est interprété en tant que int et qu'elle n'a pas été convertie au préalable,
-		# il faut alors le convertir, en rendant le nombre positif en ajoutant 32.767
+		# il faut alors le convertir, en rendant le nombre positif en ajoutant 32.767 ---> Techniquement incorrecte
 		# rawADC += 0x7FFF # Décommenter cette ligne si on récupère des nombres négatifs
 
-		# Divisé par 65535 car on veut un nombre entre 0 et 1.
-		return rawADC / 65535
+		# Divisé par 65535 car on veut un nombre entre 0 et 1. ---> Incorrecte aussi
+		# return rawADC / 65535
+		
+        # Bon, je suis allé lire la datasheet du composant et Texas Instrument nous dit qu'en positif uniquement la valeur max est 7FFFh
+		
+		# EXTRAIT DE LA DOC traduit de l'anglais
+		# Les ADS111x fournissent des données sur 16 bits au format complément à deux (binary 2’s-complement).
+        # Une entrée correspondant à l’échelle pleine positive (+FS) produit un code de sortie de 7FFFh.
+        # Une entrée correspondant à l’échelle pleine négative (–FS) produit un code de sortie de 8000h.
+        # La sortie se bloque à ces valeurs lorsque le signal dépasse l’échelle maximale.
+		
+        # Les mesures de signaux en mode entrée simple (single-ended), 
+		# où VAINN = 0V et VAINP varie de 0V à +FS, 
+		# n'utilisent que la plage de codes positifs, allant de 0000h à 7FFFh. 
+        # Cependant, en raison du décalage (offset) interne du dispositif, 
+		# l'ADS111x peut encore générer des codes négatifs lorsque VAINP est proche de 0V.
+        # FIN DE L'EXTRAIT
+		
+		# En fait en positif, l'ADS1115 compte de 0 à 32 767
+		# En négatif, l'ADS1115 décompte de 65 535 à 32 768
+		# L'ADC peut donc envoyer des données négative random si l'entrée analogique est trop proche de 0 -> Data saute de 0 à 65 535
+		# Dans notre cas on utilise une seule entrée donc on a seulement la plage positive
+		# Donc la bonne formule serait (divisé par 32 767 car on souhaite obtenir une valeur comprise entre 0 et 1)
+		return rawADC / 32767
+        # Pour avoir des valeurs négatives, il faut utiliser le mode différentiel avec deux entrées 
 
 	def GetWaterLevelCuve1(self) -> int:
-		# return self.ads.getConversionP0GND() / 32768 / 4.096
 		# return self.chan0.voltage   # raw data not affected by the gain
-		# return self.chan0.value  / 65535  / 4.096   # data affected by the gain
+		# return self.chan0.value     # data affected by the gain
 		return self.ConvertRawWaterValue(self.chan0.value)
+	
+        # DEPRECATED
+        # return self.ads.getConversionP0GND() / 32768 / 4.096
 
 	def GetWaterLevelCuve2(self) -> int:
-		# return self.ads.getConversionP1GND() / 32768 / 4.096
-		# return self.chan1.value  / 65535  / 4.096
+
 		return self.ConvertRawWaterValue(self.chan1.value)
 	
+        # DEPRECATED
+        # return self.ads.getConversionP1GND() / 32768 / 4.096
+	
 	def GetWaterLevelCuve3(self) -> int:
-		# return self.ads.getConversionP2GND() / 32768 / 4.096
-		# return self.chan2.value  / 65535  / 4.096
 		return self.ConvertRawWaterValue(self.chan2.value)
+	
+        # DEPRECATED
+		# return self.ads.getConversionP2GND() / 32768 / 4.096
