e2cf67f56ccea75b1a9885cbe9b8203da52475a7
AlphaGaming7780 <69048433+AlphaGaming7780@users.noreply.github.com>
2025-04-30 17:25:16 +0200

    rfgre

--- src/WebPage/src/RegisteredUserPanel/RegisteredUserPanel.tsx
+++ src/WebPage/src/RegisteredUserPanel/RegisteredUserPanel.tsx
@@ -1,19 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './RegisteredUserPanel.css';
 import { ChangeClientMode, ResetActiveClient, TakeControl, UpdatedClientDataContext } from '../API/ClientAPI.tsx';
 import { RadioCheckbox } from './RadioCheckbox/RadioCheckbox.tsx';
 import { ClientPanel } from './ClientPanel/ClientPanel.tsx';
-import { IsAdminContext } from '../API/Admin.tsx';
+import { IsAdminContext, RegisterAdmin } from '../API/Admin.tsx';
 
 export const RegisteredUserPanel = () => {
 
 	const { ClientEnabled, ActiveClient, ClientList } = useContext(UpdatedClientDataContext)
 	const IsAdmin = useContext(IsAdminContext)
 
+	const [password, setPassword] = useState("")
+
 	return (
 		<div className="registered-user-panel">
+			<h3>Admin Panel</h3>
 			{IsAdmin ? <>
-				<h3>Admin Panel</h3>
 				<div style={{display:"flex", flexDirection:"row", alignItems:"center"}}>
 					<button type='button' onClick={ () => ResetActiveClient() } >Reset Clients</button>
 					<button type='button' onClick={ () => TakeControl() } >Take control</button>
@@ -22,7 +24,12 @@ export const RegisteredUserPanel = () => {
 					<RadioCheckbox checked={ClientEnabled} onChange={ChangeClientMode} />
 					<p style={{paddingLeft: "0.4rem" }}>Disable/Enable clients (students)</p>
 				</span>	
-			</> : <></>
+			</> : <>
+				<span style={{display:"flex", flexDirection:"row", alignItems:"center"}}>
+					<input onChange={ (i) => {setPassword(i.target.value)}} type='password' placeholder='Password'>Password</input>
+					<button onClick={ () => RegisterAdmin(password)} style={{paddingLeft: "0.4rem" }} >Register</button>
+				</span>	
+			</>
 			}
 
 			<h3>Active Client</h3>
